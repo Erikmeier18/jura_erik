e728e4d57817838600fca19c0f854324288e5cb1
Erikmeier18 <erikmeier18@gmail.com>
2023-05-09 12:11:19 +0200

    New setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+	long_description = fh.read() 
 setuptools.setup(
 	name="rv_curve_jura",
 	version="0.1",
@@ -7,6 +9,9 @@ setuptools.setup(
 	author_email="erikmeier18@gmail.com",
 	description="Plot all RV curves that you want!",
 	packages=setuptools.find_packages(),
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Erikmeier18/jura_erik/tree/main/rv_curve_jura"
 	install_requires = ["numpy","matplotlib"],
 	classifiers=["Programming Language :: Python :: 3"],
 )
